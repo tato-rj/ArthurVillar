1ef8dd8cadd6077adaddaa5262a6e2cd56d8b54d
Arthur Villar <arthur@iMac.local>
2024-09-20 16:38:44 -0400

    Quick update

--- resources/views/recordings/play.blade.php
+++ resources/views/recordings/play.blade.php
@@ -20,7 +20,7 @@ img {
 
 @section('content')
 <section class="d-center h-100vh w-100">
-  <div id="player-container" class="animate__animated animate__fadeIn animate__slower p-5" style="max-width: 460px; display: none;">
+  <div id="player-container" class="animate__animated animate__fadeIn animate__slower p-5" style="max-width: 600px; display: none;">
     <div class="mb-3 p-1 w-100">
       {{-- <img class="shadow-lg mb-3 w-100" src="{{asset($recording->cover_path)}}"> --}}
       <h2 class="mb-2 lh-1">{{$recording->name}}</h2>
