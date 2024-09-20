ce32968f7cc88a201566f3803a2132ae519ee080
Arthur Villar <arthur@iMac.local>
2024-09-20 18:46:07 -0400

    Quick update

--- resources/views/components/pagetitle.blade.php
+++ resources/views/components/pagetitle.blade.php
@@ -2,7 +2,7 @@
 	<h1 class="text-center mb-0">{{$label}}</h1>
 
 	@isset($href)
-        <div>
+        <div class="mt-2">
             <a class="btn btn-sm btn-secondary" href="{{$href['url']}}" target="_blank">@fa(['icon' => $href['icon']]){{$href['label']}}</a>
         </div>
 	@endisset
