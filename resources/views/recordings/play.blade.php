be8f44ddfc8cf7b6fb380e9e805830e1fdcc12ba
Arthur Villar <arthur@iMac.local>
2024-09-20 16:58:51 -0400

    Quick update

--- resources/views/recordings/play.blade.php
+++ resources/views/recordings/play.blade.php
@@ -23,6 +23,9 @@ img {
   <div id="player-container" class="animate__animated animate__fadeIn animate__slower p-5" style="max-width: 600px; display: none;">
     <div class="mb-3 p-1 w-100">
       {{-- <img class="shadow-lg mb-3 w-100" src="{{asset($recording->cover_path)}}"> --}}
+      @if($recording->composed_in)
+      <h6 class="small">Composed in {{$recording->composed_in}}</h6>
+      @endif
       <h2 class="mb-2 lh-1">{{$recording->name}}</h2>
       <h6 class="mb-1">{{$recording->composer}}</h6>
       <h6 class="opacity-4">{{$recording->artist}}</h6>
