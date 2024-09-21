0ca714238e180dac0c3fba1393ca27b019406e76
Arthur Villar <arthur@iMac.local>
2024-09-21 13:44:02 -0400

    Quick update

--- resources/views/recordings/table/row.blade.php
+++ resources/views/recordings/table/row.blade.php
@@ -1,7 +1,7 @@
 @php($recording = $row)
 @switch((new \Table)->getFieldname($field))
   @case('name')
-    <span style="width: 20px; height: 20px" class="rounded-circle bg-{{$recording->color}}"></span>{{$recording->name}}
+    <span style="width: 20px; height: 20px" class="rounded-circle bg-{{$recording->period->color}}"></span>{{$recording->name}}
     @break
   
   @case('composer')
