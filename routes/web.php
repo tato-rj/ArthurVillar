daddf1f162d26d806adb1cc4e5f9e963023362fd
Arthur Villar <arthur@iMac.local>
2024-09-21 11:18:49 -0400

    Quick update

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::domain(config('app.url'))->prefix('play/{recording}')->name('recordings.')->group(function() {
+Route::middleware('token')->domain(config('app.url'))->prefix('play/{recording}')->name('recordings.')->group(function() {
     Route::get('', 'RecordingsController@play')->name('play');
 });
 
