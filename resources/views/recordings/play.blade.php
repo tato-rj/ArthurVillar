e8dcb0e298ede0a655f3f9450e08565518ff710e
Arthur Villar <arthur@iMac.local>
2024-09-20 22:49:13 -0400

    Quick update

--- resources/views/recordings/play.blade.php
+++ resources/views/recordings/play.blade.php
@@ -24,7 +24,7 @@ img {
 
 @section('content')
 <section class="d-center w-100" style="height: 80vh;">
-  <div id="player-container" class="animate__animated animate__fadeIn animate__slower p-5" style="max-width: 600px; display: none;">
+  <div id="player-container" class="animate__animated animate__fadeIn animate__slower p-5" style="width: 600px; display: none;">
     <div class="mb-3 p-1 w-100">
       {{-- <img class="shadow-lg mb-3 w-100" src="{{asset($recording->cover_path)}}"> --}}
       @if($recording->composed_in)
