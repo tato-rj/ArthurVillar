5a6e20e841f93da7397bf8c85058f532cb9a1d48
Arthur Villar <arthur@iMac.local>
2024-09-20 22:40:00 -0400

    Quick update

--- resources/views/recordings/info.blade.php
+++ resources/views/recordings/info.blade.php
@@ -1,3 +1,3 @@
 @modal(['title' => 'Info', 'id' => 'recording-'.$recording->id.'-info-modal'])
-<p style="white-space: pre-wrap;">{{$recording->description}}</p>
+<p class="m-0" style="white-space: pre-wrap;">{{$recording->description}}</p>
 @endmodal
