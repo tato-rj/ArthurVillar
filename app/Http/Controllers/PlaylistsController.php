efbd4c8c78fd2bb72e37ee2ae92faa5a808eddeb
Arthur Villar <arthur@iMac.local>
2024-09-23 15:14:24 -0400

    Quick update

--- app/Http/Controllers/PlaylistsController.php
+++ app/Http/Controllers/PlaylistsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Playlist;
 
-class PlaylistController extends Controller
+class PlaylistsController extends Controller
 {
     public function index()
     {
