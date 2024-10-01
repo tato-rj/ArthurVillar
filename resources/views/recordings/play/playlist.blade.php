a5552f85dfe2e104f9c7b2f6c158f43219b89982
Arthur Villar <arthur@iMac.local>
2024-10-01 16:38:04 -0400

    Quick update

--- resources/views/recordings/play/playlist.blade.php
+++ resources/views/recordings/play/playlist.blade.php
@@ -2,7 +2,7 @@
 <div id="playlist-container" style="display: none;" class="animate__animated animate__bounceInUp animate__slower text-center position-fixed bottom-0 left-0 w-100 border-top">
 	<button class="btn-raw p-3 w-100" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">
 			@fa(['icon' => 'chevron-up', 'mr' => 0])
-			<div class="fw-bold lh-1">Show playlist</div>
+			<div class="fw-bold lh-1 text-primary">Show playlist</div>
 	</button>
 </div>
 
@@ -14,7 +14,7 @@
   <div class="offcanvas-body px-0 pt-0 pb-5">
   	<div class="row"> 
   		<div class="col-lg-6 col-md-8 col-10 mx-auto"> 
-  			<h5 class="offcanvas-title mb-4">@fa(['icon' => 'list'])Pieces for {{$playlist->name}}</h5>
+  			<h5 class="offcanvas-title mb-4">@fa(['icon' => 'list']){{$playlist->name}}</h5>
 		    @foreach($playlist->recordings as $recording)
 		    <div class="d-apart {{$loop->last ? null : 'border-bottom mb-2 pb-2'}}">
 		    	<div>
