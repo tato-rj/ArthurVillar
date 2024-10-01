93fa48e0dacf5d039235e38eb1212c7a366553ba
Arthur Villar <arthur@iMac.local>
2024-10-01 16:00:13 -0400

    Quick update

--- resources/views/recordings/play/about.blade.php
+++ resources/views/recordings/play/about.blade.php
@@ -1,7 +1,4 @@
-@modal(['id' => 'recording-'.$recording->id.'-about-modal'])
-@slot('title')
-<div class="small opacity-6">{{$recording->composed_in}}</div>
-{{$recording->name}}
-@endslot
+@modal(['title' => $recording->name, 'id' => 'recording-'.$recording->id.'-about-modal'])
+<p class="small opacity-6">Composed in {{$recording->composed_in}}</p>
 <p class="m-0" style="white-space: pre-wrap;">{{$recording->description}}</p>
 @endmodal
