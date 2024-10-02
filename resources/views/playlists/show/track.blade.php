9f5368a70e5e26269aba71c204e0c632120ef482
Arthur Villar <arthur@iMac.local>
2024-10-02 15:22:44 -0400

    Quick update

--- resources/views/playlists/show/track.blade.php
+++ resources/views/playlists/show/track.blade.php
@@ -6,7 +6,7 @@
 	@csrf
 
 	<div {{$recording->is($playingRecording) ? null : 'submit'}} class="track-container d-apart {{$loop->last ? null : 'border-bottom mb-2 pb-2'}}">
-		<div class="d-flex align-items-center">
+		<div class="d-flex align-items-center text-truncate">
 			<h5 class="mr-2 ml-1 mb-0">{{$loop->iteration}}</h5>
 			<div class="text-truncate mr-3">
 		    	<h6 class="m-0 text-truncate">{{$recording->name}}</h6>
