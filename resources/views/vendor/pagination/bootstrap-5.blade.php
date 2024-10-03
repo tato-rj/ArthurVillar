5221a0cc7ab28e6696b62974f0bb1b0f576d2922
Arthur Villar <arthur@iMac.local>
2024-10-03 12:04:20 -0400

    Quick update

--- resources/views/vendor/pagination/bootstrap-5.blade.php
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -44,11 +44,11 @@
                     {{-- Previous Page Link --}}
                     @if ($paginator->onFirstPage())
                         <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                            <span class="page-link text-secondary" aria-hidden="true">&lsaquo;</span>
+                            <span class="page-link" aria-hidden="true">&lsaquo;</span>
                         </li>
                     @else
                         <li class="page-item">
-                            <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                            <a class="page-link text-secondary" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
                         </li>
                     @endif
 
