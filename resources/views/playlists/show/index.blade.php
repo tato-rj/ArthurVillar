72da9a252160bd7b4dec96cd243b96c28ffa5035
Arthur Villar <arthur@iMac.local>
2024-10-02 23:23:21 -0400

    Quick update

--- resources/views/playlists/show/index.blade.php
+++ resources/views/playlists/show/index.blade.php
@@ -7,14 +7,13 @@
 </div>
 
 <div class="offcanvas offcanvas-bottom border-0" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-  <div class="offcanvas-header pb-2">
-    <h5 class="offcanvas-title text-center" id="offcanvasBottomLabel"></h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
   <div class="offcanvas-body pt-0 pb-5">
   	<div class="row"> 
   		<div class="col-lg-6 col-md-8 col-12 mx-auto"> 
-  			<h5 class="offcanvas-title mb-4">@fa(['icon' => 'list']){{$playlist->name}}</h5>
+  			<div class="d-apart mb-4">
+	  			<h5 class="offcanvas-title mb-0 mr-3 text-truncate">@fa(['icon' => 'list']){{$playlist->name}}</h5>
+	  			<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+	  		</div>
 		    @foreach($playlist->recordings as $recording)
 					@include('playlists.show.track')
 		    @endforeach
