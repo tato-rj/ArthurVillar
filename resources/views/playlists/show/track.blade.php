90e170885e4a7d9ab1f691b294d6b589d986bea2
Arthur Villar <arthur@iMac.local>
2024-10-02 23:32:00 -0400

    Quick update

--- resources/views/playlists/show/track.blade.php
+++ resources/views/playlists/show/track.blade.php
@@ -20,7 +20,7 @@
 			@endfor
 		</div>
 		@else
-    	<button class="btn btn-secondary btn-sm text-nowrap" type="button">@fa(['icon' => 'play'])Play</button>
+    	<button class="btn btn-secondary btn-sm text-nowrap" type="button">@fa(['icon' => 'play', 'mr' => 0])</button>
     	@endif
 	</div>
 </form>
