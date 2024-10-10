dc126106b4a546539da31f46fd9f9f34f8ec6856
Arthur Villar <arthur@iMac.local>
2024-10-10 18:26:49 -0400

    Quick update

--- resources/views/listening/books/controls.blade.php
+++
@@ -1,6 +0,0 @@
-@if($book->tracks()->exists())
-<div id="controls" class="d-flex my-3">
-	<button id="play-all" data-label="Play all" start class="btn btn-outline-secondary w-100 mr-2">Play all</button>
-	<button id="shuffle" data-label="Shuffle" start class="btn btn-outline-secondary w-100 ml-2">Shuffle</button>
-</div>
-@endif
--- resources/views/listening/methods/create.blade.php
+++
@@ -1,9 +0,0 @@
-@modal(['title' => 'New method', 'id' => 'create-method-modal'])
-<form id="create-method" method="POST" action="{{route('listening.methods.store')}}">
-	@csrf
-	
-	@input(['placeholder' => 'Name', 'name' => 'name', 'required' => true, 'value' => old('name')])
-
-	@submit(['label' => 'Submit', 'theme' => 'primary'])
-</form>
-@endmodal
