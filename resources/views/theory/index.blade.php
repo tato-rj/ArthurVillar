b53c9ab9b0d7b2fafd3e6e34f8a6d8b2e20e60ed
Arthur Villar <arthur@iMac.local>
2026-02-07 00:31:09 -0500

    Quick update

--- resources/views/theory/index.blade.php
+++ resources/views/theory/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app', ['noMenu' => true])
 
 @push('header')
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 <style>
   :root{
     /* Staff box */
