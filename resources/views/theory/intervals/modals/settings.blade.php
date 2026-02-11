506257b96d15c72d7997faca31c57cad111573f1
Arthur Villar <arthur@iMac.local>
2026-02-11 11:37:36 -0500

    Quick update

--- resources/views/theory/intervals/modals/settings.blade.php
+++ resources/views/theory/intervals/modals/settings.blade.php
@@ -12,9 +12,9 @@
 	<div class="d-apart mb-3">
 		<label class="nowrap">Number of rounds</label>
 		<div class="d-center form-number">
-			<button type="button" data-direction="down" class="btn-raw">@fa(['icon' => 'minus-circle', 'fa_color' => 'muted', 'mr' => 0, 'fa_size' => 'lg'])</button>
+			<button style="touch-action: manipulation;" type="button" data-direction="down" class="btn-raw">@fa(['icon' => 'minus-circle', 'fa_color' => 'muted', 'mr' => 0, 'fa_size' => 'lg'])</button>
 			<input type="text" readonly name="rounds" max="12" min="2" value="4" class="form-control form-control-sm py-1 mx-2" style="width: 52px;">
-			<button type="button" data-direction="up" class="btn-raw">@fa(['icon' => 'plus-circle', 'fa_color' => 'muted', 'mr' => 0, 'fa_size' => 'lg'])</button>
+			<button style="touch-action: manipulation;" type="button" data-direction="up" class="btn-raw">@fa(['icon' => 'plus-circle', 'fa_color' => 'muted', 'mr' => 0, 'fa_size' => 'lg'])</button>
 		</div>
 {{-- 		<select style="width: fit-content;" name="rounds" class="form-select form-select-sm py-1">
 			@for($i=2; $i<=12; $i++)
