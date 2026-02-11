de76c3d266d77904d9fa4e51ef7a0e57cf03b79d
Arthur Villar <arthur@iMac.local>
2026-02-11 11:39:47 -0500

    Quick update

--- resources/views/theory/intervals/modals/settings.blade.php
+++ resources/views/theory/intervals/modals/settings.blade.php
@@ -12,9 +12,9 @@
 	<div class="d-apart mb-3">
 		<label class="nowrap">Number of rounds</label>
 		<div class="d-center form-number">
-			<button style="touch-action: manipulation;" type="button" data-direction="down" class="btn-raw">@fa(['icon' => 'minus', 'mr' => 0, 'fa_size' => 'xl'])</button>
+			<button style="touch-action: manipulation;" type="button" data-direction="down" class="btn-raw text-dark">@fa(['icon' => 'minus', 'mr' => 0, 'fa_size' => 'xl'])</button>
 			<input type="text" readonly name="rounds" max="12" min="2" value="4" class="form-control form-control-sm py-1 mx-2 text-center" style="width: 52px;">
-			<button style="touch-action: manipulation;" type="button" data-direction="up" class="btn-raw">@fa(['icon' => 'plus', 'mr' => 0, 'fa_size' => 'xl'])</button>
+			<button style="touch-action: manipulation;" type="button" data-direction="up" class="btn-raw text-dark">@fa(['icon' => 'plus', 'mr' => 0, 'fa_size' => 'xl'])</button>
 		</div>
 {{-- 		<select style="width: fit-content;" name="rounds" class="form-select form-select-sm py-1">
 			@for($i=2; $i<=12; $i++)
