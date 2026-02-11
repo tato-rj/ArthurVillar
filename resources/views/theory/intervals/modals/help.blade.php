ef39299143feb98d1b6f763c29be4057817d0593
Arthur Villar <arthur@iMac.local>
2026-02-11 11:51:46 -0500

    Quick update

--- resources/views/theory/intervals/modals/help.blade.php
+++ resources/views/theory/intervals/modals/help.blade.php
@@ -1,18 +1,25 @@
 @modal(['title' => 'Help', 'id' => 'help-modal'])
 <p>Each round shows an interval name (like M3 or P5) and gives you a starting note on either the treble or bass clef.</p> 
 
-<div class="p-2 bg-light mb-2">
-<h6>Notes</h6>
-<p class="m-0"><strong>Tap</strong> the staff to place your note and <strong>drag</strong> it to adjust.</p>
+<div class="mb-4">
+	<h6>Notes</h6>
+	<div class="bg-light p-2">
+	<strong>Tap</strong> the staff to place your note and <strong>drag</strong> it to adjust.
+	</div>
 </div>
 
-<div class="p-2 bg-light mb-2">
-<h6>Accidentals</h6>
-<p class="m-0"><strong>Drag and drop</strong> sharps/flats/naturals on the note when needed.</p> 
+<div class="mb-4">
+	<h6>Accidentals</h6>
+	<div class="bg-light p-2">
+		<strong>Drag and drop</strong> sharps/flats/naturals on the note when needed. 
+		For double sharps or double flats, just drag and drop twice.
+	</div>
 </div>
 
-<div class="p-2 bg-light mb-2">
-<h6>Confirm your answer</h6>
-<p class="m-0">Hit <strong>Check</strong> to see if you got it right, earn points (with a bonus for first-try answers), and continue through the set.</p>
+<div class="mb-">
+	<h6>Confirm your answer</h6>
+	<div class="bg-light p-2">
+		Hit <strong>Check</strong> to see if you got it right, earn points (with a bonus for first-try answers), and continue through the set.
+	</div>
 </div>
 @endmodal
