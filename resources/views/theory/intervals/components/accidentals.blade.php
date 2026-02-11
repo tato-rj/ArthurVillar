c51d5577c2fe6096386277e791f81cb03a845f52
Arthur Villar <arthur@iMac.local>
2026-02-11 12:17:26 -0500

    Quick update

--- resources/views/theory/intervals/components/accidentals.blade.php
+++ resources/views/theory/intervals/components/accidentals.blade.php
@@ -1,9 +1,9 @@
     <div id="accidentals" class="mt-">
       {{-- <label class="opacity-6" style="font-size: 70%;">Drag and drop</label> --}}
       {{-- <div  class="d-center"> --}}
-        <label class="music-font music-font__sharp mx-2"></label>
+        <label class="music-font music-font__sharp p-2"></label>
         {{-- <label class="music-font music-font__doublesharp"></label> --}}
-        <label class="music-font music-font__flat mx-2"></label>
+        <label class="music-font music-font__flat p-2"></label>
         {{-- <label class="music-font music-font__doubleflat"></label> --}}
         {{-- <label class="music-font music-font__natural mx-3"></label> --}}
       {{-- </div> --}}
