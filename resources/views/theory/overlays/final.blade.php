4ce7ef183c5b4878df7307e25a19e9853613b8ad
Arthur Villar <arthur@iMac.local>
2026-02-14 13:34:51 -0500

    Quick update

--- resources/views/theory/overlays/final.blade.php
+++ resources/views/theory/overlays/final.blade.php
@@ -1,9 +1,20 @@
 <div id="final-overlay" class="position-absolute w-100 top-0 left-0">
 	<div class="text-center container" style="margin-top: 2rem;">
-		<img src="{{asset('images/badge.png')}}" width="140" class="animate__animated animate__zoomInDown mb-4">
-		<div class="text-center animate__animated animate__fadeInUp animate__faster mb-4">
-			<h1 class="text-">Great job!</h1>
-			<h6 class="text-muted px-5">It's not about getting the most points, but if it was...</h6>
+		<div class="position-relative">
+			<div class="position-absolute top-0 left-0 w-100 h-100 d-center">
+				<dotlottie-wc
+				  class="w-100"
+				  style="z-index: 0;"
+				  src="https://lottie.host/5c852ec4-3960-4cbb-b1aa-45926b3d4545/O4zf2YSwRs.lottie"
+				  autoplay
+				  loop
+				></dotlottie-wc>
+			</div>
+			<img src="{{asset('images/badge.png')}}" width="140" class="animate__animated animate__zoomInDown mb-4 pt-3 pr-2">
+			<div class="text-center animate__animated animate__fadeInUp animate__faster mb-4">
+				<h1 class="text-">Great job!</h1>
+				<h6 class="text-muted px-5">It's not about getting the most points, but if it was...</h6>
+			</div>
 		</div>
 
 		<div class="row mb-4">
