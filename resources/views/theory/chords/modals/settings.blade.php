705548b977fb5422d916cef6414461e23c9f9a0f
Arthur Villar <arthur@iMac.local>
2026-02-20 09:09:49 -0500

    Quick update

--- resources/views/theory/chords/modals/settings.blade.php
+++ resources/views/theory/chords/modals/settings.blade.php
@@ -26,8 +26,8 @@
     <div class="d-flex flex-wrap">
       @foreach($challenge->getClefs() as $clef)
       <div class="m-1 position-relative">
-        <input name="clefs[]" type="checkbox" value="{{$clef}}" class="invisible position-absolute top-0 left-0" id="{{$clef}}-clef" {{in_array($clef, $challenge->options('clefs')) ? 'checked' : null}} autocomplete="off">
-        <label class="btn btn-{{in_array($clef, $challenge->options('clefs')) ? 'secondary' : 'white'}}" for="{{$clef}}-clef">{{ucfirst($clef)}}</label>
+        <input name="clefs[]" type="checkbox" value="{{$clef}}" class="invisible position-absolute top-0 left-0" id="{{$clef}}-clef-{{$modalID ?? null}}" {{in_array($clef, $challenge->options('clefs')) ? 'checked' : null}} autocomplete="off">
+        <label class="btn btn-{{in_array($clef, $challenge->options('clefs')) ? 'secondary' : 'white'}}" for="{{$clef}}-clef-{{$modalID ?? null}}">{{ucfirst($clef)}}</label>
       </div>
       @endforeach
     </div>
