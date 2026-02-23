83126065a4397b4e4c10d93788c4dd9587449321
Arthur Villar <arthur@iMac.local>
2026-02-23 10:26:00 -0500

    Quick update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,12 +28,12 @@
                     @fa(['icon' => 'youtube', 'fa_type' => 'b', 'fa_size' => 'lg', 'mr' => 0])
                 </a>
             </div>
-
+{{-- 
             <div class="mx-2">
                 <a href="https://www.instagram.com/piano_with_arthur/" target="_blank">
                     @fa(['icon' => 'instagram', 'fa_type' => 'b', 'fa_size' => 'lg', 'mr' => 0])
                 </a>
-            </div>
+            </div> --}}
 
             <div class="mx-2">
                 <a href="https://www.facebook.com/arthur.villar.31" target="_blank">
