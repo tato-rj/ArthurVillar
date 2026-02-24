da619a667b5febd6bda07832b9c82d1bb65c14fa
Arthur Villar <arthur@iMac.local>
2026-02-24 10:02:00 -0500

    Quick update

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,22 @@ Route::get('', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/analytics/opt-out', function (Request $request) {
-    return redirect('/')
-        ->withCookie(cookie(
-            name: 'exclude_analytics',
-            value: '1',
-            minutes: 60 * 24 * 365 * 5, // 5 years
-            path: '/',
-            domain: null,
-            secure: true,
-            httpOnly: false,
-            raw: false,
-            sameSite: 'Lax'
-        ));
-});
+// Route::get('/analytics/opt-out', function (Request $request) {
+//     return redirect('/')
+//         ->withCookie(cookie(
+//             name: 'exclude_analytics',
+//             value: '1',
+//             minutes: 60 * 24 * 365 * 5, // 5 years
+//             path: '/',
+//             domain: null,
+//             secure: true,
+//             httpOnly: false,
+//             raw: false,
+//             sameSite: 'Lax'
+//         ));
+// });
 
-Route::get('/analytics/opt-in', function () {
-    return redirect('/')
-        ->withCookie(cookie()->forget('exclude_analytics'));
-});
+// Route::get('/analytics/opt-in', function () {
+//     return redirect('/')
+//         ->withCookie(cookie()->forget('exclude_analytics'));
+// });
