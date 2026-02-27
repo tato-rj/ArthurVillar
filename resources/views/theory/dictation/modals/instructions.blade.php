79e454071c9230275ac0c0d2ad57aa79fbdcf3b1
Arthur Villar <arthur@iMac.local>
2026-02-26 23:08:31 -0500

    Quick update

---
+++ resources/views/theory/dictation/modals/instructions.blade.php
@@ -0,0 +1,21 @@
+@modal(['title' => fa('book').' Instructions', 'id' => 'instructions-modal'])
+<div class="p-3 mb-3 bg-light rounded border">
+<p>Each round plays <strong>two notes</strong> in sequence.</p>
+<p class="m-0">Listen carefully and <strong>write the second note you hear</strong> on the staff, above or below the given note.</p>
+</div>
+
+<div class="mb-3 pb-3 border-bottom">
+	<h6><u>How to add a note?</u></h6>
+	<p class="m-0"><strong>Tap</strong> the staff to place a note, then <strong>drag</strong> to adjust it.</p>
+</div>
+
+<div class="mb-3 pb-3 border-bottom">
+	<h6><u>How to add accidentals?</u></h6>
+	<p class="m-0"><strong>Drag and drop</strong> sharps, flats, or naturals onto the note. For double sharps or flats, drag and drop twice.</p>
+</div>
+
+<div class="">
+	<h6><u>What do I do if I’m stuck?</u></h6>
+	<p class="m-0">If you make a mistake, a <strong>help button</strong> appears. Click it to see the answer. <u>Using help will make the round be worth 0 points</u>.</p>
+</div>
+@endmodal
