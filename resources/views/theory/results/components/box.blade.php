f93785d6628a2b598aba38986192bfee97f9cb84
Arthur Villar <arthur@iMac.local>
2026-02-27 23:41:25 -0500

    Quick update

--- resources/views/theory/results/components/box.blade.php
+++ resources/views/theory/results/components/box.blade.php
@@ -1,5 +1,5 @@
 <div class="col-6 animate__animated animate__bounceIn">
-	<div class="w-100 bg-light border border-dark {{$color}} position-relative rounded p-1 mb-3" style="border-radius: 16px !important;">
+	<div class="w-100 border border-dark bg-{{$color}} position-relative rounded p-1 mb-3" style="border-radius: 16px !important;">
 		@if($name == 'score')
 		<div id="double-points">
 			<div class="d-center bg-primary animate__animated animate__tada border border-black position-relative">
@@ -13,8 +13,8 @@
 			</div>
 		</div>
 		@endif
-		<div class="pb-1 text-center fw-bold opacity-6" style="font-size: 70%">{{$title}}</div>
-		<div class="text-{{$color}} border border-dark bg-white rounded fw-bold d-center px-2 py-3 h5 m-0" style="border-radius: 16px !important;">
+		<div class="pb-1 text-center fw-bold" style="font-size: 70%">{{$title}}</div>
+		<div class="text-{{$color}} border border-dark bg-{{$color}}-lighter rounded fw-bold d-center px-2 py-3 h5 m-0" style="border-radius: 16px !important;">
 			@fa <span name="{{$name}}"></span>
 		</div>
 	</div>
