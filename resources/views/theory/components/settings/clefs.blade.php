6a0fa79f8739d305fd627df87ff8b7b75bcadade
Arthur Villar <arthur@iMac.local>
2026-03-03 14:10:16 -0500

    Quick update

--- resources/views/theory/components/settings/clefs.blade.php
+++ resources/views/theory/components/settings/clefs.blade.php
@@ -3,8 +3,8 @@
     <div class="d-flex flex-wrap">
       @foreach($settings->getClefs() as $clef)
       <div class="m-1 position-relative">
-        <input name="clefs[]" type="checkbox" value="{{$clef}}" class="invisible position-absolute top-0 left-0" id="{{$clef}}-clef-{{$modalID ?? null}}" {{in_array($clef, $settings->options('clefs')) ? 'checked' : null}} autocomplete="off">
-        <label class="btn btn-{{in_array($clef, $settings->options('clefs')) ? 'secondary' : 'white'}}" for="{{$clef}}-clef-{{$modalID ?? null}}">{{ucfirst($clef)}}</label>
+        <input name="clefs[]" type="checkbox" value="{{$clef}}" class="invisible position-absolute top-0 left-0" id="{{$clef}}-clef-{{str_slug($settings->gameName())}}" {{in_array($clef, $settings->options('clefs')) ? 'checked' : null}} autocomplete="off">
+        <label class="btn btn-{{in_array($clef, $settings->options('clefs')) ? 'secondary' : 'white'}}" for="{{$clef}}-clef-{{str_slug($settings->gameName())}}">{{ucfirst($clef)}}</label>
       </div>
       @endforeach
     </div>
--- resources/views/theory/components/settings/intervals.blade.php
+++ resources/views/theory/components/settings/intervals.blade.php
@@ -3,8 +3,8 @@
     <div class="d-flex flex-wrap">
       @foreach($settings->getIntervals() as $interval)
       <div class="m-1 position-relative">
-        <input name="intervals[]" type="checkbox" value="{{$interval}}" class="invisible position-absolute top-0 left-0" id="{{$modalID ?? null}}-{{$interval}}" {{in_array($interval, $settings->options('intervals')) ? 'checked' : null}} autocomplete="off">
-        <label class="btn btn-{{in_array($interval, $settings->options('intervals')) ? 'secondary' : 'white'}}" for="{{$modalID ?? null}}-{{$interval}}">{{$interval}}</label>
+        <input name="intervals[]" type="checkbox" value="{{$interval}}" class="invisible position-absolute top-0 left-0" id="{{str_slug($settings->gameName())}}-{{$interval}}" {{in_array($interval, $settings->options('intervals')) ? 'checked' : null}} autocomplete="off">
+        <label class="btn btn-{{in_array($interval, $settings->options('intervals')) ? 'secondary' : 'white'}}" for="{{str_slug($settings->gameName())}}-{{$interval}}">{{$interval}}</label>
       </div>
       @endforeach
     </div>
