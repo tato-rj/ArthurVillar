f2e9af3b03a9769fd202fd7b03c8130e0da7c1ca
Arthur Villar <arthur@iMac.local>
2026-05-13 14:03:36 -0400

    Quick update

--- routes/admin.php
+++ routes/admin.php
@@ -12,6 +12,8 @@ Route::prefix('leaderboards')->name('leaderboard.')->group(function() {
 
 	Route::post('fake', 'LeaderboardsController@fake')->name('fake');
 
+	Route::get('{player}', 'LeaderboardsController@edit')->name('edit');
+
 	Route::delete('{player}', 'LeaderboardsController@destroy')->name('destroy');
 });
 
