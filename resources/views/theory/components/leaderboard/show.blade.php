92f48fe83910488dbe923e7fde529daee2d08a8f
Arthur Villar <arthur@iMac.local>
2026-05-18 19:06:09 -0400

    Quick update

--- resources/views/theory/components/leaderboard/show.blade.php
+++ resources/views/theory/components/leaderboard/show.blade.php
@@ -1,11 +1,13 @@
 @modal(['title' => fa('ranking-star').'Leaderboard', 'class' => 'leaderboard', 'id' => 'leaderboard-'.str_slug($settings->gameName()).'-modal', 'autoshow' => (bool) session('newPlayer')])
-@forelse($settings->leaderboard() as $player)
-	@include('theory.components.leaderboard.player')
-@empty
-<div class="p-5 d-center">
-	<div>
-		<p class=" fst-italic text-grey opacity-6 fw-bold">No players yet...</p>
+<div style="max-height: 600px; overflow-y: scroll;" class="p-2">
+	@forelse($settings->leaderboard() as $player)
+		@include('theory.components.leaderboard.player')
+	@empty
+	<div class="p-5 d-center">
+		<div>
+			<p class=" fst-italic text-grey opacity-6 fw-bold">No players yet...</p>
+		</div>
 	</div>
+	@endforelse
 </div>
-@endforelse
 @endmodal
