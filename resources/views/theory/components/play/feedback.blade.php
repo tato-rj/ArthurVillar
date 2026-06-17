1758ddcfc7bdb161138fb802055e697665091391
Arthur Villar <arthur@iMac.local>
2026-06-17 09:41:31 -0400

    Quick update

--- resources/views/theory/components/play/feedback.blade.php
+++ resources/views/theory/components/play/feedback.blade.php
@@ -1,4 +1,4 @@
-<div id="play-feedback" class="px-2 py-1 rounded">
+<div id="play-feedback" class="px-2 py-1 rounded-sm">
 	<div class="d-center">
 		@fa(['icon' => 'microphone', 'mr' => 1])
 	</div>
