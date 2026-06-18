4215b1a44ee9bcfa71b41b94b8f717f655260ddd
Arthur Villar <arthur@iMac.local>
2026-06-17 22:46:15 -0400

    Quick update

--- resources/views/theory/components/controls.blade.php
+++ resources/views/theory/components/controls.blade.php
@@ -2,20 +2,20 @@
   <div class="d-flex flex-column">
 
     @isset($instructions)
-    <div class="position-relative mb-3">
-      <div id="instructions" class="fw-bold text-center">
+    <div class="position-relative">
+      <div id="instructions" class="fw-bold text-center mb-3">
         <h6 class="m-0 text-grey" style="line-height: 1.4">{{$instructions ?? null}} </h6>
       </div>
-      <div id="check" class="btn-floating w-100 invisible" style="display: none">
+      <div id="check" class="btn-floating w-100 invisible mb-3" style="display: none">
         <button class="btn btn-primary w-100">Check my answer</button>
       </div>
-      <div id="help" class="btn-floating w-100" style="display: none">
+      <div id="help" class="btn-floating w-100 mb-3" style="display: none">
         <button class="btn btn-blue w-100">@fa(['icon' => 'wand-magic-sparkles'])See answer</button>
       </div>
-      <div id="skip" class="btn-floating w-100" style="display: none;">
+      <div id="skip" class="btn-floating w-100 mb-3" style="display: none;">
         <button class="btn btn-red w-100">Next round</button>
       </div>
-      <div id="play-note" class="btn-floating w-100 invisible" style="display: none">
+      <div id="play-note" class="btn-floating w-100 invisible mb-3" style="display: none">
         <button class="btn btn-blue w-100">@fa(['icon' => 'microphone'])Tap here and play the note</button>
       </div>
     </div>    
