e12ab01975b51f250ad43ef61a73997231aee37f
Arthur Villar <arthur@iMac.local>
2026-07-07 17:00:08 -0400

    Quick update

--- resources/views/studio/lessonPlans/card.blade.php
+++ resources/views/studio/lessonPlans/card.blade.php
@@ -17,10 +17,18 @@
 
 		@if($current)
 		<div class="mb-3">
-			<div class="mb-1">@fa(['icon' => 'piggy-bank', 'fa_color' => 'grey']){{$lessonPlan->payment_method}}</div>
-			<div class="mb-1">@fa(['icon' => 'money-bill-wave', 'fa_color' => 'grey']){{payment()->usd($lessonPlan->fee_amount)}}</div>
-			<div class="mb-1">@fa(['icon' => 'repeat', 'fa_color' => 'grey']){{$lessonPlan->recurrence}}</div>
-			<div>@fa(['icon' => 'clock', 'fa_color' => 'grey']){{$lessonPlan->startTime()->format('g:i A')}}</div>
+			<div class="mb-1">
+				@fa(['icon' => 'piggy-bank', 'fa_color' => 'grey']){{$lessonPlan->payment_method}}
+			</div>
+			<div class="mb-1">
+				@fa(['icon' => 'money-bill-wave', 'fa_color' => 'grey']){{payment()->usd($lessonPlan->fee_amount)}}
+			</div>
+			<div class="mb-1">
+				@fa(['icon' => 'repeat', 'fa_color' => 'grey']){{$lessonPlan->recurrence}}
+			</div>
+			<div>
+				@fa(['icon' => 'clock', 'fa_color' => 'grey']){{$lessonPlan->startTime()->format('g:i A')}}
+			</div>
 		</div>
 		@endif
 
