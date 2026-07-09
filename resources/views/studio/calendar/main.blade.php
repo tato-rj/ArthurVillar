399f8759c247e210b8c53a607405fa969cff38b6
Arthur Villar <arthur@iMac.local>
2026-07-09 07:30:43 -0400

    Quick update

--- resources/views/studio/calendar/main.blade.php
+++ resources/views/studio/calendar/main.blade.php
@@ -1,7 +1,7 @@
 <div class="studio-calendar-main">
 	<div class="studio-calendar-toolbar" aria-label="Calendar controls">
 
-		<div class="btn-group">
+{{-- 		<div class="btn-group">
 			<button type="button" class="studio-calendar-icon-button" data-calendar-prev aria-label="Previous">
 				<i class="fa-solid fa-chevron-left" aria-hidden="true"></i>
 			</button>
@@ -11,15 +11,15 @@
 			<button type="button" class="studio-calendar-icon-button" data-calendar-next aria-label="Next">
 				<i class="fa-solid fa-chevron-right" aria-hidden="true"></i>
 			</button>
-		</div>
-
-{{-- 		<div class="studio-calendar-toolbar-today" style="flex: 0 0 auto;">
-			<button type="button" class="btn btn-white btn-wide studio-calendar-today" data-calendar-today>
-				Today
-			</button>
 		</div> --}}
 
-{{-- 		<div class="flex-grow-1 d-flex align-items-center studio-calendar-toolbar-controls">
+		<div class="studio-calendar-toolbar-today" style="flex: 0 0 auto;">
+			<button type="button" class="btn btn-white studio-calendar-today" data-calendar-today>
+				Today
+			</button>
+		</div>
+
+		<div class="flex-grow-1 d-flex align-items-center studio-calendar-toolbar-controls">
 			<div class="studio-calendar-nav mr-2" aria-label="Calendar navigation">
 				<button type="button" class="studio-calendar-icon-button" data-calendar-prev aria-label="Previous">
 					<i class="fa-solid fa-chevron-left" aria-hidden="true"></i>
@@ -30,7 +30,7 @@
 			</div>
 
 			<h4 class="studio-calendar-label" data-calendar-label></h4>
-		</div> --}}
+		</div>
 
 		<div class="studio-calendar-toolbar-views" style="flex: 0 0 auto;">
 			<label class="studio-calendar-view">
