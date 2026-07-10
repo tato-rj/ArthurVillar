a27cb692fa0e97f5ab15600703d2cadbc1045168
Arthur Villar <arthur@iMac.local>
2026-07-10 09:40:45 -0400

    Quick update

--- resources/views/studio/calendar/sidebar/insights.blade.php
+++ resources/views/studio/calendar/sidebar/insights.blade.php
@@ -2,23 +2,23 @@
 
 	<div id="studio-calendar-insights" class="">
 		<div class="mb-3">
-			<h5 class="m-0" data-calendar-lessons-count>0</h5>
-			<label class="small">LESSONS</label>
+			<h6 class="m-0" data-calendar-lessons-count>0</h6>
+			<label style="font-size: .75em;">LESSONS</label>
 		</div>
 		<div class="mb-3">
-			<h5 class="m-0" data-calendar-hours-count>0h</h5>
-			<label class="small">HOURS</label>
+			<h6 class="m-0" data-calendar-hours-count>0h</h6>
+			<label style="font-size: .75em;">HOURS</label>
 		</div>
 		<div class="mb-3" style="display: none;">
-			<h5 class="m-0" data-calendar-average-hours>0/day</h5>
-			<label class="small">AVERAGE</label>
+			<h6 class="m-0" data-calendar-average-hours>0/day</h6>
+			<label style="font-size: .75em;">AVERAGE</label>
 		</div>
 		<div class="mb-3">
 			<div class="d-flex align-items-center">
-				<h5 class="m-0" data-calendar-confirmed-payment>$0</h5>
+				<h6 class="m-0" data-calendar-confirmed-payment>$0</h6>
 				<span class="small fw-bold mx-2 opacity-4">/</span>
 				<p class="m-0 opacity-4" data-calendar-expected-payment>$0</p>
 			</div>
-			<label class="small">INCOME</label>
+			<label style="font-size: .75em;">INCOME</label>
 		</div>
 	</div>
