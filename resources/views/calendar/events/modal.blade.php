223b272c756fbac6e63338ddc5f9ac1cc097d1d3
Arthur Villar <arthur@iMac.local>
2026-07-22 23:36:02 -0400

    Quick update

--- resources/views/calendar/events/modal.blade.php
+++ resources/views/calendar/events/modal.blade.php
@@ -32,7 +32,7 @@
         <span id="general-event-notification"></span>
     </div>
 
-    <div data-general-event-notes-section hidden class="calendar-modal-detail-section mb-3">
+    <div data-general-event-notes-section hidden class="calendar-modal-detail-section">
         <div class="small font-weight-bold opacity-6 mb-2 mt-3">NOTES</div>
         <div id="general-event-notes" class="calendar-general-event-notes"></div>
     </div>
