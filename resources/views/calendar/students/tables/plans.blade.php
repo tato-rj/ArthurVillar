e0a75ea3cfd02a55b93233db4c87cc7ea9b827e6
Arthur Villar <arthur@iMac.local>
2026-08-14 17:52:10 -0400

    Quick update

--- resources/views/calendar/students/tables/plans.blade.php
+++ resources/views/calendar/students/tables/plans.blade.php
@@ -2,26 +2,25 @@
 @unless($recurringLessonPlans->isEmpty())
     <h5 class="mb-3">Recurring Lesson Plans</h5>
 
-    @foreach($recurringLessonPlans as $lessonPlan)
-    {{$lessonPlan->id}}
-{{--         <div class="border rounded p-4 mb-3">
-            <div class="{{$loop->last ? null : 'border-bottom pb-3 mb-3'}}">
-                <div class="small font-weight-bold text-{{$lessonPlan->isCurrent() ? 'green' : 'light'}} mb-1">{{$lessonPlan->isCurrent() ? 'CURRENT' : 'PAST'}}</div>
-                <div class="font-weight-bold">
-                    {{ucfirst($lessonPlan->weekday_name)}} at {{\App\Models\Calendar\LessonPlan::timeLabel($lessonPlan->start_time)}}
-                </div>
-                <div class="small opacity-6">
-                    {{$lessonPlan->recurrence}}
-                    @if($lessonPlan->starts_on && $lessonPlan->ends_on)
-                    · {{$lessonPlan->starts_on->format('M j, Y')}}–{{$lessonPlan->ends_on->format('M j, Y')}}
-                    @endif
-                </div>
-                @if($lessonPlan->location)
-                <div class="small mt-1">@fa(['icon' => 'location-dot', 'classes' => 'opacity-4']){{$lessonPlan->location->name}}</div>
+    <div class="border rounded p-4 mb-3">
+    @foreach($recurringLessonPlans as $lessonPlan)        
+        <div class="{{$loop->last ? null : 'border-bottom pb-3 mb-3'}}">
+            <div class="small font-weight-bold text-{{$lessonPlan->isCurrent() ? 'green' : 'light'}} mb-1">{{$lessonPlan->isCurrent() ? 'CURRENT' : 'PAST'}}</div>
+            <div class="font-weight-bold">
+                {{ucfirst($lessonPlan->weekday_name)}} at {{\App\Models\Calendar\LessonPlan::timeLabel($lessonPlan->start_time)}}
+            </div>
+            <div class="small opacity-6">
+                {{$lessonPlan->recurrence}}
+                @if($lessonPlan->starts_on && $lessonPlan->ends_on)
+                · {{$lessonPlan->starts_on->format('M j, Y')}}–{{$lessonPlan->ends_on->format('M j, Y')}}
                 @endif
             </div>
-        </div> --}}
+            @if($lessonPlan->location)
+            <div class="small mt-1">@fa(['icon' => 'location-dot', 'classes' => 'opacity-4']){{$lessonPlan->location->name}}</div>
+            @endif
+        </div>
     @endforeach
+    </div>
 @endunless
 
 @unless($singleLessonsPlans->isEmpty())
