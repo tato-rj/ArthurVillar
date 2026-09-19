73d3c6b70628e069af2015330564e86a64274861
Arthur Villar <arthur@iMac.local>
2026-09-19 13:10:28 -0400

    Quick update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,6 @@ class UsersController extends Controller
 {
     public function index()
     {
-        return User::all();
         $users = User::query()
             ->where('email', '!=', User::ARTHUR_EMAIL)
             ->withCount('schedulers')
